91a8ca4d56e796031c3637e237df6c28559017bb
admin <1252419379@qq.com>
2019-05-17 11:36:18 +0800

    tring

---
+++ Example/Pods/Target Support Files/NSCateStringSubKit/NSCateStringSubKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NSCateStringSubKit : NSObject
+@end
+@implementation PodsDummy_NSCateStringSubKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-NSCateStringSubKit_Tests/Pods-NSCateStringSubKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NSCateStringSubKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NSCateStringSubKit_Tests
+@end
